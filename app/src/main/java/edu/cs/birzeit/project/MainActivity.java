36d050bce610348f8582415796d5825146dc201a
sondos <https://github.com/sondosquraan/example1.git>
2022-05-13 15:09:33 +0300

    v 1.0

---
+++ app/src/main/java/edu/cs/birzeit/project/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.cs.birzeit.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btnSubmit_Click(View view) {
+
+    }
+}
---
+++ app/src/main/java/edu/cs/birzeit/project/location.java
@@ -0,0 +1,14 @@
+package edu.cs.birzeit.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class location extends AppCompatActivity {
+}
